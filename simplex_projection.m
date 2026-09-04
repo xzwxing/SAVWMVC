19761cb04d8dbc6cf28eca2cd59c5e7e993e96f0
xzwxing <57620035+xzwxing@users.noreply.github.com>
2026-09-04 15:38:21 +0800

    Document source of simplex projection algorithm
    Added literature reference for the simplex projection algorithm.

--- simplex_projection.m
+++ simplex_projection.m
@@ -1,4 +1,8 @@
 function X = simplex_projection(Y)
+% The code is obtained from literature: 
+% Wang W, Carreira-Perpinan M A. Projection onto the probability simplex: An
+% efficient algorithm with a simple proof, and an application. arXiv:1309.1541, Sept. 3 2013.
+
 %The output of X satisfies sum(X(i,:))=1,i=1,2,...,N; 
 [N,D] = size(Y);
 X = sort(Y,2,'descend');
